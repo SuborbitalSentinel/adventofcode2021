015884029b03952bb2ab8b155f44010e57aabb1d
Adam Miller <millera023@gmail.com>
2021-12-01 22:52:15 -0500

    Day1:Part2 -- Complete

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,16 @@
-use std::collections::HashMap;
+// 199 -- 0: 0
+// 200 -- 1: 0 1
+// 208 -- 2: 0 1 2
+// 210 -- 3: 1 2 3
+// 200 -- 4: 4 2 3
+// 207 -- 5: 4 5 3
+// 240 -- 6: 4 5 6
+// 269 -- 7: 5 6 7
+// 260 -- 8: 6 7
+// 263 -- 9: 7
+// 0 + 1 + 2 < 1 + 2 + 3 => 0 < 3
+// 1 + 2 + 3 < 2 + 3 + 4 => 1 < 4
+
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -27,29 +39,22 @@ fn part_1() -> i32 {
         previous_number = *number;
     }
 
-    return number_of_increases;
+    number_of_increases
 }
 
-// fn part_2() -> i32 {
-//     let lines = as_ints(read_file("test.txt"));
-//     let mut groups = HashMap::new();
-//     for (i, number) in lines.iter().enumerate() {
-//         *groups.entry(i/3).or_insert(*number) += number;
-//     }
-
-//     let mut number_of_increases = 0;
-//     let mut previous_number = groups[&0];
-//     for number in groups.values().skip(1) {
-//         if number > &previous_number {
-//             number_of_increases += 1;
-//         }
-//         previous_number = *number;
-//     }
-
-//     return number_of_increases;
-// }
+fn part_2() -> i32 {
+    let lines = as_ints(read_file("input.txt"));
+    let max_lines = lines.len();
+    let mut number_of_increases = 0;
+    for (i, number) in lines.iter().enumerate() {
+        if (i + 3) < max_lines && number < &lines[i + 3] {
+            number_of_increases += 1;
+        }
+    }
+    number_of_increases
+}
 
 fn main() {
     println!("Number of increases part1: {:?}", part_1());
-    // println!("Number of increases part1: {:?}", part_2());
+    println!("Number of increases part2: {:?}", part_2());
 }
