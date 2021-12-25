1d86f9d8476e7792846ecbd96f52be2809ace225
Adam Miller <millera023@gmail.com>
2021-12-24 23:37:06 -0500

    Day7: Part 1 Complete

---
+++ src/bin/day7.rs
@@ -0,0 +1,37 @@
+use std::ops::RangeInclusive;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+pub fn atoi(input: &str) -> isize {
+    input.parse::<isize>().unwrap()
+}
+
+fn part_1(filename: &str) -> isize {
+    let mut line = String::new();
+    let _ = BufReader::new(File::open(filename).expect("file not found")).read_line(&mut line);
+
+    let initial_crabs: Vec<_> = line.trim_end().split(",").map(|v| atoi(v)).collect();
+    let max_position = initial_crabs.iter().max().unwrap().clone();
+    let number_of_crabs = initial_crabs.len();
+
+    let mut possible_positions = vec![];
+    RangeInclusive::new(0, max_position).for_each(|pos| possible_positions.push(vec![pos; number_of_crabs]));
+
+    let mut minimum_edit_distance = isize::MAX;
+    possible_positions.iter().for_each(|x| {
+        let edit_distance: isize = initial_crabs.iter().zip(x).map(|(l, r)| (l-r).abs()).sum();
+        if edit_distance < minimum_edit_distance {
+            minimum_edit_distance = edit_distance;
+        }
+    });
+
+    return minimum_edit_distance;
+}
+
+fn main() {
+    assert_eq!(part_1("input/7_test.txt"), 37);
+    println!("Part 1: {:?}", part_1("input/7_input.txt"));
+    // assert_eq!(part_2("input/6_test.txt"), 26984457539);
+    // println!("Part 2: {:?}", part_2("input/6_input.txt"));
+}
